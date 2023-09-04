374bd32a5c983079bfec3f645690d94481187baf
sinmc8987 <142969271+sinmc8987@users.noreply.github.com>
2023-09-04 15:49:35 +0900

    Update hello.js
    fix typo
    
    - -> =
    en -> end

--- hello.js
+++ hello.js
@@ -2,10 +2,10 @@ const http = require('http');
 const os = require('os');
 console.log("Test server starting...");
 
-var handler - function(request, response) {
+var handler = function(request, response) {
    console.log("Received request from " + request.connection.remoteAddress);
    response.writeHead(200);
-   response.en("Container Hostname: " + os.hostname() + "\n");
+   response.end("Container Hostname: " + os.hostname() + "\n");
 };
 
 var www = http.createServer(handler);
